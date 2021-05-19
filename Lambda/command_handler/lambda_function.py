74d03e93e23198f3a1a584dcd4857b79c042c95f
maxization <damian.opoka99@gmail.com>
2021-05-20 00:16:43 +0200

    Add commands in command_handler

--- Lambda/command_handler/lambda_function.py
+++ Lambda/command_handler/lambda_function.py
@@ -1,8 +1,51 @@
 import json
 import boto3
 
+
+def set_param(param):
+    # TODO: Sends to SQS that set information
+    print(param['name'])
+
+
+def account_handler(event):
+    options = event['data']['options']
+    action = options[0]['name']
+    if action == 'delete':
+        # TODO: Sends to SQS that delete information from database
+        print("Deleting...")
+    elif action == 'set':
+        set_options = options[0]['options']
+        for param in set_options:
+            set_param(param)
+
+
+def review_handler(event):
+    options = event['data']['options']
+    content = options[0]['value']
+    # TODO: Sends to SQS that analyze message
+    print(content)
+
+
+def recommend_handler(event):
+    number = 0
+    if 'options' in event['data']:
+        number = event['data']['options'][0]['value']
+    else:
+        number = 1
+    # TODO: Sends to SQS with recommendations
+    print(f"Recommed {number}")
+
+
 def lambda_handler(event, context):
-    print("debug")
+    command = event['data']['name']
+
+    if command == 'account':
+        account_handler(event)
+    elif command == 'review':
+        review_handler(event)
+    elif command == 'recommend':
+        recommend_handler(event)
+
     lambda_client = boto3.client('lambda')
     lambda_client.invoke(FunctionName='chat_service',
                          InvocationType='Event',
